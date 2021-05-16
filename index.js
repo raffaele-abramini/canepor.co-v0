5eb4657aeb4168a6fbc755e4970f5ed300e476ea
oldirony <raffaele.abramini@gmail.com>
2021-05-16 18:28:49 +0100

    :construction: Add autogen logic

---
+++ index.js
@@ -0,0 +1,13 @@
+const init = async () => {
+  const data = await fetch("https://generatore-service.raff.ae/");
+  const content = await data.text();
+
+  const args = JSON.parse(content);
+
+  const elem = document.getElementById("target");
+
+  elem.innerText = args.join(" ")
+};
+
+
+init();
