8ab00d67fdf16a8398f59cfaec798838ecefb44a
oldirony <raffaele.abramini@gmail.com>
2021-05-17 21:49:56 +0100

    :construction: Add button logic

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const init = async () => {
+const fetchAndPopulate = async (target) => {
   const data = await fetch("https://generatore-service.raff.ae/");
   const content = await data.text();
 
   const args = JSON.parse(content);
 
-  const elem = document.getElementById("target");
+  target.innerText = args.join(" ")
+  target.classList.add("active")
+}
 
-  elem.innerText = args.join(" ")
-  elem.classList.add("active")
+const init = async () => {
+  const target = document.getElementById("target");
+  const button = document.getElementById("button")
+
+  button.addEventListener("click", async () => {
+    target.classList.remove("active")
+    button.disabled = true;
+    await fetchAndPopulate(target);
+  })
+  await fetchAndPopulate(target);
 };
 
 
